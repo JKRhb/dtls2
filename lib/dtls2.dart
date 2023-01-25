2afe01c868762c8a2b8c4d3425a0ad5522d58507
Jan Romann <jan.romann@uni-bremen.de>
2023-01-25 14:45:47 +0100

    fix: export DtlsException class

--- lib/dtls2.dart
+++ lib/dtls2.dart
@@ -4,6 +4,7 @@
 
 export 'src/dtls_client.dart';
 export 'src/dtls_connection.dart';
+export 'src/dtls_exception.dart';
 export 'src/dtls_server.dart';
 export 'src/openssl_load_exception.dart';
 export 'src/psk_credentials.dart';
